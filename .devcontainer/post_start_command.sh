d9de7ff9a05e841c33f4cc018726c3c0939eea7f
mysubsnewslivecom <my.subs.news@live.com>
2024-04-23 23:41:08 +0200

    feat: add semantic-release to post_start_commands.sh

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -4,3 +4,17 @@ export VIRTUAL_ENV=$HOME/venv
 python3 -m venv $VIRTUAL_ENV
 export PATH="$VIRTUAL_ENV/bin:$PATH"
 python3 -m pip install mkdocs-techdocs-core
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+bash -c -l 'nvm install 20'
+
+ npm install semantic-release \
+    @semantic-release/gitlab \
+    @semantic-release/github \
+    @semantic-release/changelog \
+    @semantic-release/git \
+    @semantic-release/exec \
+    @semantic-release/commit-analyzer \
+    conventional-changelog-conventionalcommits \
+    @semantic-release/gitlab
